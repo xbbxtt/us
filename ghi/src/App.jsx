0e776b220910a5dcfc5931eadb3dea4b78253a29
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-05-06 18:21:47 -0400

    adding styling to signup and nav

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
             <header className="App-header">
                 <Nav />
             </header>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
             <Outlet />
             <footer>
                 <h3>Footer....</h3>
--- ghi/src/components/SignUpForm.jsx
+++ ghi/src/components/SignUpForm.jsx
@@ -23,13 +23,19 @@ export default function SignInForm() {
         setValues(newValues)
     }
 
-
     async function handleFormSubmit(e) {
-        console.log("**********************")
+        console.log('**********************')
         e.preventDefault()
         signup({
-            username, password, first_name, last_name,
-            location, gender, age, description, picture_url,
+            username,
+            password,
+            first_name,
+            last_name,
+            location,
+            gender,
+            age,
+            description,
+            picture_url,
         })
     }
 
@@ -45,91 +51,108 @@ export default function SignInForm() {
     useEffect(() => {
         getGender()
         handleChange()
-        if (signupStatus.isSuccess){
-            console.log("success")
-            signin({username, password})
-            navigate('/romantic-pref')}
+        if (signupStatus.isSuccess) {
+            console.log('success')
+            signin({ username, password })
+            navigate('/romantic-pref')
+        }
     }, [signupStatus, navigate])
 
-
     return (
-        <form className="text-black" onSubmit={handleFormSubmit}>
-            <input
-                type="text"
-                name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Enter Username"
-                className="mx-2"
-            />
-            <input
-                type="text"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter Password"
-                className="mx-2"
-            />
-            <input
-                type="text"
-                name="first_name"
-                value={first_name}
-                onChange={(e) => setFirst_name(e.target.value)}
-                placeholder="First Name"
-                className="mx-2"
-            />
-            <input
-                type="text"
-                name="last_name"
-                value={last_name}
-                onChange={(e) => setLast_name(e.target.value)}
-                placeholder="Last Name"
-                className="mx-2"
-            />
-            <input
-                type="text"
-                name="location"
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-                placeholder="Enter Your Location"
-                className="mx-2"
-            />
-            <select value={gender} onChange={(e) => setGender(e.target.value)}>
-                <option value="">Select your gender</option>
-                {genders.length > 0 &&
-                    genders.map((gender) => {
-                        return (
-                            <option key={gender.id} value={gender.id}>
-                                {gender.gender_name}
-                            </option>
-                        )
-                    })}
-            </select>
-
-            <input
-                type="number"
-                name="age"
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
-                className="mx-2"
-            />
-            <input
-                type="text"
-                name="description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder="Describe Yourself"
-                className="mx-2"
-            />
-            <input
-                type="url"
-                name="picture_url"
-                value={picture_url}
-                onChange={(e) => setPicture_url(e.target.value)}
-                placeholder="Enter Picture URL"
-                className="mx-2"
-            />
-            <button type="submit">Sign Up!</button>
-        </form>
+        <div className="flex mx-2 border border-gray-300 rounded-md px-3 py-4">
+            <form
+                className="text-black flex flex-wrap"
+                onSubmit={handleFormSubmit}
+            >
+                <div className="w-full md:w-1/2 py-3 px-3">
+                    <input
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Enter Username"
+                        className="mx-2  border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter Password"
+                        className="mx-2 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    />
+                    <input
+                        type="text"
+                        name="first_name"
+                        value={first_name}
+                        onChange={(e) => setFirst_name(e.target.value)}
+                        placeholder="First Name"
+                        className="mx-2 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    />
+                    <input
+                        type="text"
+                        name="last_name"
+                        value={last_name}
+                        onChange={(e) => setLast_name(e.target.value)}
+                        placeholder="Last Name"
+                        className="mx-2 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    />
+                    <input
+                        type="text"
+                        name="location"
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                        placeholder="Enter Your Location"
+                        className="mx-2 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    />
+                </div>
+                <div className="w-full md:w-1/2 py-3 px-3">
+                    <select
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)}
+                        className="mx-2 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    >
+                        <option value="">Select your gender</option>
+                        {genders.length > 0 &&
+                            genders.map((gender) => {
+                                return (
+                                    <option key={gender.id} value={gender.id}>
+                                        {gender.gender_name}
+                                    </option>
+                                )
+                            })}
+                    </select>
+                    <input
+                        type="number"
+                        name="age"
+                        value={age}
+                        onChange={(e) => setAge(e.target.value)}
+                        className="mx-2 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    />
+                    <input
+                        type="text"
+                        name="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder="Describe Yourself"
+                        className="mx-2 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    />
+                    <input
+                        type="url"
+                        name="picture_url"
+                        value={picture_url}
+                        onChange={(e) => setPicture_url(e.target.value)}
+                        placeholder="Enter Picture URL"
+                        className="mx-2 border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full md:w-auto"
+                >
+                    Sign Up!
+                </button>
+            </form>
+        </div>
     )
 }
