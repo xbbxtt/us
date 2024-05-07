c7a8346c75ca89b48519764ac8a8ea656836221e
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-05-07 10:21:50 -0400

    fixed the nav bar

--- ghi/src/components/Nav.jsx
+++ ghi/src/components/Nav.jsx
@@ -19,14 +19,14 @@ const Nav = () => {
     }
 
     return (
-        <nav className="bg-pink-300 py-4 fixed top-0 left-0 w-full">
-            <div className="mx-auto max-w-7xl px-4">
-                <ul className="flex justify-between">
+        <nav className="bg-pink-300 py-2 fixed top-0 left-0 w-full">
+            <div className="mx-auto max-w-7xl px-2">
+                <ul className="flex items-start mx-2">
                     {!user && (
                         <li>
                             <NavLink
                                 to={'/'}
-                                className="text-white hover:text-gray-300"
+                                className="text-white hover:text-gray-300 px=4"
                             >
                                 Home
                             </NavLink>
@@ -36,7 +36,7 @@ const Nav = () => {
                         <li>
                             <NavLink
                                 to={'/signup'}
-                                className="text-white hover:text-gray-300"
+                                className="text-white hover:text-gray-300 px-4"
                             >
                                 Sign Up
                             </NavLink>
@@ -56,7 +56,7 @@ const Nav = () => {
                         <li>
                             <NavLink
                                 to={'/romantic-pref'}
-                                className="text-white hover:text-gray-300"
+                                className="text-white hover:text-gray-300 px-4"
                             >
                                 Romantic Preferences
                             </NavLink>
@@ -66,7 +66,7 @@ const Nav = () => {
                         <li>
                             <NavLink
                                 to={'/likes'}
-                                className="text-white hover:text-gray-300"
+                                className="text-white hover:text-gray-300 px-4"
                             >
                                 Likes
                             </NavLink>
@@ -76,7 +76,7 @@ const Nav = () => {
                         <li>
                             <NavLink
                                 to={'/romantic-preferences'}
-                                className="text-white hover:text-gray-300"
+                                className="text-white hover:text-gray-300 px-4"
                             >
                                 Get to swiping!
                             </NavLink>
@@ -86,18 +86,21 @@ const Nav = () => {
                         <li>
                             <NavLink
                                 to={'/matches'}
-                                className="text-white hover:text-gray-300"
+                                className="text-white hover:text-gray-300 px-4"
                             >
                                 Matches
                             </NavLink>
                         </li>
                     )}
+                    {user && (
+                        <button
+                            className="btn bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded-md ml-auto mt-1"
+                            onClick={onSignoutClick}
+                        >
+                            Logout
+                        </button>
+                    )}
                 </ul>
-                {user && (
-                    <button className="btn mt-4" onClick={onSignoutClick}>
-                        Logout
-                    </button>
-                )}
             </div>
         </nav>
     )
