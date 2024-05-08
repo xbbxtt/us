7de4b26e5f645134f84842fb490439006448ec0c
Yadriel <147550067+Yadriel-Ruiz@users.noreply.github.com>
2024-05-08 13:17:47 -0500

    working on css for swiping

--- ghi/src/components/Swiping.jsx
+++ ghi/src/components/Swiping.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react'
-import { useGendersQuery, useGetAllPotentialLikesQuery, useCreateLikeMutation } from '../app/apiSlice'
+import {
+    useGendersQuery,
+    useGetAllPotentialLikesQuery,
+    useCreateLikeMutation,
+} from '../app/apiSlice'
+import '../css/cards.css'
+// import '../js/swiping.js'
 
 export default function RomanticPreferences() {
     const [potentialLikes, setPotentialLikes] = useState([])
@@ -8,8 +14,6 @@ export default function RomanticPreferences() {
     const potentialLikesQuery = useGetAllPotentialLikesQuery()
     const [createLike] = useCreateLikeMutation()
     const [isLoading, setIsLoading] = useState(false)
-
-    // fetch users by preferences according to the user that is logged in
 
     useEffect(() => {
         if (gendersQuery.data) {
@@ -28,13 +32,17 @@ export default function RomanticPreferences() {
         try {
             const data = {
                 logged_in_user: likedUserId,
-                liked_by_user: 1, 
+                liked_by_user: 1,
                 status: null,
             }
             console.log('Data:', data)
             const response = await createLike(data)
             console.log('Response:', response)
             if (response.data && response.data.id) {
+                // Filter out the liked card from the potentialLikes array
+                setPotentialLikes(
+                    potentialLikes.filter((like) => like.id !== likedUserId)
+                )
                 console.log('Success')
             } else {
                 console.log('Error sending like')
@@ -47,40 +55,37 @@ export default function RomanticPreferences() {
 
     return (
         <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="tinder--cards">
                 {potentialLikes.map((like) => (
-                    <div
-                        className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
-                        key={like.id}
-                    >
+                    <div className="tinder--card" key={like.id}>
                         <img
                             src={like.picture}
                             alt={like.username}
                             className="rounded-t-lg"
                         />
-                        <div className="p-5">
-                            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                {like.first_name} - {like.age}
-                            </h2>
-                            <h3>
-                                {genders.map((gender) => {
-                                    if (gender.id === like.gender) {
-                                        return gender.gender_name
-                                    }
-                                    return null
-                                })}
-                            </h3>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                {like.description}
-                            </p>
-
+                        <h3 className="">
+                            {like.first_name} - {like.age}
+                        </h3>
+                        <h4>
+                            {genders.map((gender) => {
+                                if (gender.id === like.gender) {
+                                    return gender.gender_name
+                                }
+                                return null
+                            })}
+                        </h4>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            {like.description}
+                        </p>
+                        <div className="tinder--buttons">
                             <button
-                                className="flex-1 inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none"
+                                id="love"
+                                className="fa fa-heart"
                                 onClick={() => handleLike(like.id)}
                             >
                                 Like
                             </button>
-                            <button className="flex-1 inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none">
+                            <button id="nope" className="fa fa-remove">
                                 Dislike
                             </button>
                         </div>
@@ -90,4 +95,3 @@ export default function RomanticPreferences() {
         </div>
     )
 }
-
