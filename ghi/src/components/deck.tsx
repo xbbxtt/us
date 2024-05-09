fba5130dd5f9beb30210e03a58f9cae16f9db34f
Yadriel <147550067+Yadriel-Ruiz@users.noreply.github.com>
2024-05-09 14:34:32 -0500

    fully functioning swipe

--- ghi/src/components/deck.tsx
+++ ghi/src/components/deck.tsx
@@ -21,7 +21,6 @@ const trans = (r, s) =>
 
 export default function Deck() {
     const [cards, setCards] = useState([])
-    const [potentialLikes, setPotentialLikes] = useState([])
     const [genders, setGenders] = useState([])
     const gendersQuery = useGendersQuery({})
     const potentialLikesQuery = useGetAllPotentialLikesQuery({})
@@ -58,15 +57,18 @@ export default function Deck() {
             direction: [xDir],
             velocity,
         }) => {
-            const trigger = velocity > 0.2
-            const dir = xDir < 0 ? -1 : 1
+            console.log('direction:', xDir)
+            
+            const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
+            const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
 
             // Animation logic
-            const x = dir * Math.min(1, mx / window.innerWidth)
-            const rot = mx / 100 + (gone.has(index) ? dir * 10 * velocity : 0)
-            const scale = down ? 1.1 : 1
+            const isGone = gone.has(index)
+            const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0 // When a card is gone it flies out left or right, otherwise goes back to zero
+            const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
+            const scale = down ? 1.1 : 1 // Active cards lift up a bit
             api.start((i) => {
-                if (index !== i) return
+                if (index !== i) return // We're only interested in changing spring-data for the current spring
                 return {
                     x,
                     rot,
@@ -74,15 +76,16 @@ export default function Deck() {
                     delay: undefined,
                     config: {
                         friction: 50,
-                        tension: down ? 800 : gone.has(index) ? 200 : 500,
+                        tension: down ? 800 : isGone ? 200 : 500,
                     },
                 }
             })
 
             // Handle like/dislike
             if (!down && trigger) {
-                setGone((prev) => new Set(prev.add(index)))
-                if (dir === 1) {
+                setGone((prev) => new Set(prev.add(index))) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+                if (dir > .5) {
+                    // Only trigger like action if swiping right
                     handleLike(cards[index].id, index) // Swipe right
                 }
                 // Wait for the animation duration before removing the card from the UI
@@ -94,6 +97,8 @@ export default function Deck() {
             }
         }
     )
+
+
 
 
 
